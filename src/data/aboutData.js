1c04533c372072910b65687071de8969c3c06d92
swayam143 <shubhamswl@gmail.com>
2024-04-28 16:52:25 +0530

    change

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,8 @@
 export const aboutData = {
   title: "Introduction",
   description1:
-    "My name is Shubham Oswal. I'm a Senior React designer and developer (knows mern as well) based in Indore, MP.",
+    "My name is Shubham Oswal. I'm a Senior React developer based in Indore, MP. I also have expertise in the MERN stack.",
+
   description2:
     "During the day, I work as a Senior React developer at Facillima Technology. In the evening, I focus on React projects and dedicate time to building my own products. I spend my leisure hours researching and developing new technologies. Currently, I'm also honing my backend skills. I have a passion for learning and exploring new technologies. At Facillima Technology, I serve as a Senior React JS developer, leading and mentoring a team of React JS developers. In this role, I take pride in guiding my fellow developers, fostering collaboration, and facilitating knowledge sharing. I regularly conduct knowledge transfer sessions and code reviews to uphold high standards of coding practices within the team",
   image: 2,
